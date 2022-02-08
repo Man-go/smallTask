256265b0af149919ae7397dc94dcce5f7bc403ba
Severin <insmer@gmail.com>
2022-02-08 14:30:03 +0500

    Task4

--- src/job4j/level1/collectionList/Task4.java
+++ src/job4j/level1/collectionList/Task4.java
@@ -6,6 +6,7 @@ import java.util.List;
  * метод принимает список и элемент который мы ищем в списке. Необходимо определить,
  * является ли этот элемент уникальным в этом списке.
  */
+
 public class Task4 {
     public static boolean checkList(List<String> list, String str) {
         int firstIndex = list.indexOf(str);
