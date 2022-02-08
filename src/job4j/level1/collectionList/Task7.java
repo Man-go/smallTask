799d4b75546da9a6bbf0ef07f084cf2876ff27f7
Severin <insmer@gmail.com>
2022-02-08 15:18:24 +0500

    Task7

---
+++ src/job4j/level1/collectionList/Task7.java
@@ -0,0 +1,22 @@
+package job4j.level1.collectionList;
+
+import java.util.List;
+
+/**
+ * метод принимает 2 списка и элемент, который возможно содержится в этих списках.
+ * Вам необходимо проверить оба списка, содержат ли они одновременно этот элемент,
+ * если содержит - необходимо удалить элемент из первого списка. Потом второй список
+ * нужно добавить в первый. Метод должен вернуть индекс элемента, который пришел в
+ * параметрах метода из общего списка.
+ */
+
+public class Task7 {
+
+    public static int containsElement(List<String> left, List<String> right, String str) {
+        if (left.contains(str) && right.contains(str)) {
+            left.remove(str);
+        }
+        left.addAll(right);
+        return left.indexOf(str);
+    }
+}
