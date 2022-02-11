52577ec1d33a2c09da503ab05d62a9ef58867cce
Severin <insmer@gmail.com>
2022-02-12 02:11:22 +0500

    Task2

---
+++ src/job4j/level1/sort/Task2.java
@@ -0,0 +1,12 @@
+package job4j.level1.sort;
+
+import java.util.Comparator;
+
+/**
+ * адача получить компаратор естественного порядка для строки.
+ */
+public class Task2 {
+    public static Comparator<String> naturalOrder() {
+        return Comparator.naturalOrder();
+    }
+}
