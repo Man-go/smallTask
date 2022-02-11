d559199574d564920388b8b9a37b5d402d38e487
Severin <insmer@gmail.com>
2022-02-12 02:12:58 +0500

    Task3

---
+++ src/job4j/level1/sort/Task3.java
@@ -0,0 +1,12 @@
+package job4j.level1.sort;
+
+import java.util.Comparator;
+
+/**
+ * задача получить обратный компаратор для строки
+ */
+public class Task3 {
+    public static Comparator<String> reverseOrder() {
+        return Comparator.reverseOrder();
+    }
+}
