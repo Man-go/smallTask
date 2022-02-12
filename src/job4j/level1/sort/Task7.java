408365231d6f01aaa0f5591f6527a33321611c73
Severin <insmer@gmail.com>
2022-02-12 11:26:44 +0500

    Task7

---
+++ src/job4j/level1/sort/Task7.java
@@ -0,0 +1,13 @@
+package job4j.level1.sort;
+
+import java.util.Arrays;
+
+/**
+ * отсортировать переданный массив и вернуть его
+ */
+public class Task7 {
+    public static long[] sort(long[] data) {
+        Arrays.sort(data);
+        return data;
+    }
+}
