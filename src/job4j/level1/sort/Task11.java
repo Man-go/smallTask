6918a9c6f93e155483172671e2fb09033c69d043
Severin <insmer@gmail.com>
2022-02-12 12:07:29 +0500

    Task11

---
+++ src/job4j/level1/sort/Task11.java
@@ -0,0 +1,15 @@
+package job4j.level1.sort;
+
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+/**
+ * Ваша задача получить отсортированный сет по списку строк. Среди элементов нет null.
+ */
+public class Task11 {
+    public static Set<String> sort(List<String> list) {
+        Set<String> sortedSet = new TreeSet<>(list);
+        return sortedSet;
+    }
+}
